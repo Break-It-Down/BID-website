34a4dbadef36dcfc600596210457df0fdc380236
Christie Lum <christielum02@gmail.com>
2023-08-09 19:25:34 -0700

    Basic upcoming events layout

--- src/components/upcomingEvents.js
+++ src/components/upcomingEvents.js
@@ -13,19 +13,21 @@ const UpcomingEvents = () => (
             <div class="col-4 col-12-mobile special">
                 {/* Add links for schools once groups is created */}
                 <a href="#" class="image featured"><img src={westCoast} alt="west coast host"/></a>
-                <h3><a href="#">BiDLA</a></h3>
-                <p>
-                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-                </p>
+                <h2 class="h2-banner"><a href="/groups">BiDLA</a></h2>
+                <h3>
+                Host: UCLA Road to Damascus <br/>
+                Location: Los Angeles, California <br/>
+                Date: TBD
+                </h3>
             </div>
             <article class="col-4 col-12-mobile special">
                 <a href="#" class="image featured"><img src={eastCoast} alt="east coast host" /></a>
-                <h3><a href="#">BiD?</a></h3>
-                <p>
-                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-                </p>
+                <h2 class="h2-banner"><a href="/groups">BiD?</a></h2>
+                <h3>
+                Host: Could be your group! <br/>Message us if you’re interested! <br/>
+                Location: TBD <br/>
+                Date: TBD
+                </h3>
             </article>
         </div>
     </div>
