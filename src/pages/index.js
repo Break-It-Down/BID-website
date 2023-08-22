684ef02b28b420842486c8c189312c253506f2b7
Julie Wang <juliewang@Julies-MacBook-Pro.local>
2023-08-22 13:04:20 -0400

    styling website colors and font

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,40 @@
-import * as React from "react"
-import Seo from "../components/seo"
+import * as React from "react";
+import Seo from "../components/seo";
 // import * as styles from "../components/index.module.css"
-import "../styles/index.module.css"
-import Header from "../components/header"
-import Banner from "../components/banner"
-import Carousel from "../components/carousel"
-import UpcomingEvents from "../components/upcomingEvents"
-import Footer from "../components/footer"
-import Layout from "../components/layout"
-import NumbersBanner from "../components/numbers-banner"
+import "../styles/index.module.css";
+import Header from "../components/header";
+import Banner from "../components/banner";
+import Carousel from "../components/carousel";
+import UpcomingEvents from "../components/upcomingEvents";
+import Footer from "../components/footer";
+import Layout from "../components/layout";
+import NumbersBanner from "../components/numbers-banner";
+import { useEffect } from "react";
 
-const IndexPage = () => (
-	<div>
-		<Header home="true"/>
-		<Banner/>
-		<UpcomingEvents/>
-		<NumbersBanner/>
-		<Carousel/>
-		<Footer/>
-	</div>
-)
+/*
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Header home="true" />
+      <Banner />
+      <UpcomingEvents />
+      <NumbersBanner />
+      <Carousel />
+      <Footer />
+    </div>
+  );
+};
 
 /**
  * Head export to define metadata for the page
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Home" />;
 
-export default IndexPage
+export default IndexPage;
