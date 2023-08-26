4c1b0ce3b73d219e4b941f5427f80f201f0b577d
Christie Lum <christielum02@gmail.com>
2023-08-26 04:34:44 -0700

    Fixed safari card flipper

--- src/components/CardFlipper.js
+++ src/components/CardFlipper.js
@@ -14,21 +14,9 @@ import classNames from "classnames";
 class ReactCardFlipper extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      isFlipped: false
-    };
   }
 
-  handleFlip = () => {
-    this.setState(state => ({
-      isFlipped: !state.isFlipped
-    }));
-  };
-
   render() {
-    const { isFlipped } = this.state;
-
     return (
       <div className="flip-card">
         <div className="flip-card-inner">
@@ -40,30 +28,6 @@ class ReactCardFlipper extends Component {
           </div>
         </div>
       </div>
-
-    
-      // <div className="rcf-container"
-
-      //   style={{width: this.props.width, height: this.props.height}}
-        
-      //   onMouseEnter={() => {
-      //     this.handleFlip();
-      //   }}
-      //   onMouseLeave={() => {
-      //     this.handleFlip();
-      //   }}
-      // >
-      //   <div className="rcf-flipper">
-      //     <div className="rcf-front">
-      //       {!isFlipped ? this.props.children[0] : null}
-      //       {/* {this.props.children[0]} */}
-      //     </div>
-      //     <div className="rcf-back">
-      //       {/* {isFlipped ? this.props.children[1] : null} */}
-      //       {this.props.children[1]}
-      //     </div>
-      //   </div>
-      // </div>
       );
   }
 }
