e82c07d4091a6a46f3dbeece75f7711ab291f056
Christie Lum <christielum02@gmail.com>
2023-08-26 05:47:54 -0700

    Fixed carousel height and image height bug

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -5,8 +5,6 @@ import "../styles/carousel.css"
 // import 'react-multi-carousel/lib/styles.css';
 import { StaticImage } from "gatsby-plugin-image"
 import PastEvents from "../content/pastEvents"
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 // Feed past events content into divs
 const pastEventsArray = [];
@@ -14,11 +12,11 @@ for (var i = 0; i < PastEvents.length; i ++) {
     let curr = PastEvents[i];
     pastEventsArray.push(
         <article>
-            <div class="iframe-container">
+            <div className="iframe-container">
                 {curr.youtube ? 
-                        <iframe class="responsive-iframe" src={curr.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe className="responsive-iframe" src={curr.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     :
-                    <a href="#" class="responsive-iframe"><StaticImage src="../images/pic01.jpg" alt="" /></a>
+                    <StaticImage src="../images/pic01.jpg" className="responsive-iframe"/>
                 }
             </div>
             <header>
@@ -29,33 +27,13 @@ for (var i = 0; i < PastEvents.length; i ++) {
   );
 }
 
-// Carousel functionality
-let currentFilter = 0;
-
-const length = pastEventsArray.length;
-// How many you want to see at a time
-const offSet = 3;
-const nextFilter = () => {
-    currentFilter = currentFilter === length - 1 ? 0 : currentFilter + 1;
-    console.log(currentFilter);
-};
-
-const prevFilter = () => {
-    currentFilter = currentFilter === 0 ? length - 1 : currentFilter - 1;
-    console.log(currentFilter);
-};
-
 const Carousel = () => (
-    <div class="past-events-wrapper">
+    <div className="past-events-wrapper">
         <h2><strong>Past Events</strong></h2>
-        <div class="carousel">
-            {/* <ChevronLeftIcon class="backward" onClick={prevFilter}/> */}
-            <div class="reel">
+        <div className="carousel">
+            <div className="reel">
                 {pastEventsArray}
-                {/* {pastEventsArray.filter(
-                    (d, i) => i >= currentFilter && i < currentFilter + offSet)} */}
             </div>
-            {/* <ChevronRightIcon class="forward" onClick={nextFilter}/> */}
         </div>
     </div>
 )
