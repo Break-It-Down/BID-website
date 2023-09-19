8f3d84b48f30f372847e3ad0c659b702e61e8220
Christie Lum <christielum02@gmail.com>
2023-09-19 13:49:25 -0700

    Added photo to meet groups banner

--- src/components/meet-groups-banner.js
+++ src/components/meet-groups-banner.js
@@ -2,10 +2,12 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { Button } from "@mui/material"
 import "../styles/banner.css"
+import { StaticImage } from "gatsby-plugin-image"
 
 const MeetGroups = () => (
     <div id="banner">
         <h2 class="h2-banner">Meet the Groups</h2>
+        <StaticImage src="../images/meetGroupsImage.jpg" class="banner-image"/>
         <h3>
         Comprised of 25 groups from all over the country, Break it Down strives to 
         share the gospel and good news of Jesus Christ through this niche art of Christian a cappella.
