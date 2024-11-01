7b871b04405358e86937ba1f106d8941b94e87e4
Christie Lum <christielum02@gmail.com>
2024-11-01 01:32:46 -0700

    Import images for slideshow header

--- src/content/slideshowImages.js
+++ src/content/slideshowImages.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
 import '../styles/imageSlider.css';
+import image1 from '../images/slideshow/DSC09452.jpg';
+import image2 from '../images/slideshow/IMG6059.jpg';
+import image3 from '../images/slideshow/IMG6066.jpg';
+import image4 from '../images/slideshow/IMG6067.jpg';
+import image5 from '../images/slideshow/DSC09594.jpg';
+import image6 from '../images/slideshow/IMG6056.jpg';
+import image7 from '../images/slideshow/IMG6061.jpg';
+import image8 from '../images/slideshow/IMG6069.jpg';
+import image9 from '../images/slideshow/IMG6062.jpg';
+import image10 from '../images/slideshow/IMG6063.jpg';
+import image11 from '../images/slideshow/IMG6064.jpg';
+import image12 from '../images/slideshow/IMG6065.jpg';
+import image13 from '../images/slideshow/DSC09532.jpg';
+import image14 from '../images/slideshow/IMG6068.jpg';
 
 export const MeetGroupsImages = [
     {ImageSrc: <StaticImage src="../images/meetGroups/meetGroupsImage.jpg" className='imageStyle' />},
@@ -11,12 +25,16 @@ export const MeetGroupsImages = [
     {ImageSrc: <StaticImage src="../images/meetGroups/DSC01449.jpg" className='imageStyle' />}
 ];
 
-export const HeaderImages = [
-    "/images/slideshow/DSC09452.jpg", "/images/slideshow/IMG6059.jpg", 
-    "/images/slideshow/IMG6066.jpg", "/images/slideshow/IMG6067.jpg",
-    "/images/slideshow/DSC09594.jpg", "/images/slideshow/IMG6056.jpg",
-    "/images/slideshow/IMG6061.jpg", "/images/slideshow/IMG6069.jpg",
-    "/images/slideshow/IMG6062.jpg", "/images/slideshow/IMG6063.jpg",
-    "/images/slideshow/IMG6064.jpg", "/images/slideshow/IMG6065.jpg",
-    "/images/slideshow/DSC09532.jpg","/images/slideshow/IMG6068.jpg"
+export const HeaderImages = [image1, image2, image3, image4, image5, image6, image7, image8, image9,
+     image10, image11, image12, image13, image14
 ];
+
+// export const HeaderImages = [
+//     "../images/slideshow/DSC09452.jpg", "../images/slideshow/IMG6059.jpg", 
+//     "../images/slideshow/IMG6066.jpg", "../images/slideshow/IMG6067.jpg",
+//     "../images/slideshow/DSC09594.jpg", "../images/slideshow/IMG6056.jpg",
+//     "../images/slideshow/IMG6061.jpg", "../images/slideshow/IMG6069.jpg",
+//     "../images/slideshow/IMG6062.jpg", "../images/slideshow/IMG6063.jpg",
+//     "../images/slideshow/IMG6064.jpg", "../images/slideshow/IMG6065.jpg",
+//     "../images/slideshow/DSC09532.jpg","../images/slideshow/IMG6068.jpg"
+// ];
