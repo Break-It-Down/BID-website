744b77004f34febe6035fb89ef1165888bf5468d
Kevin K. Pho (OmnInfinity, Triphone) <omninfinity687@gmail.com>
2025-08-26 23:47:26 -0400

    fix(design): merged co-hosted event
    [Implementation] [Design] [Update] Changed grid to merge the events for the co-hosts [Todo] Switch to flexbox or grid and use proportions/groupings

--- src/components/upcomingEvents.js
+++ src/components/upcomingEvents.js
@@ -11,24 +11,19 @@ const UpcomingEvents = () => (
         <div class="row aln-center">
             <div class="col-5 col-12-mobile special">
                 <a href="/groups#hosts" class="image"><StaticImage aspectRatio={16/9} src='../images/groups/UPitt.jpeg' class="hosts-photo"/></a>
-                <h2 class="h2-banner"><a href="/groups">BIDsburgh</a></h2>
-                <h3>
-                    UPitt Mosaic <br/> -- <br/>
-                    Pittsburgh, Pennsylvania <br/>
-                    April 11, 2026 (TBD)
-                </h3>
             </div>
             <div class="col-5 col-12-mobile special">
                 <a href="/groups#hosts" class="image"><StaticImage aspectRatio={16/9} src="../images/groups/CMU.JPG" class="hosts-photo"/></a>
-                <h2 class="h2-banner"><a href="/groups">BIDsburgh</a></h2>
-                <h3>
-                    CMU Joyful Noise <br/> -- <br/>
-                    Pittsburgh, Pennsylvania <br/>
-                    April 11, 2026 (TBD)
-                </h3>
             </div>
         </div>
         <div class="row aln-center">
+            <h2 class="h2-banner"><a href="/groups">BIDsburgh</a></h2>
+            <h3>
+                UPitt Mosaic + CMU Joyful Noise <br/> -- <br/>
+                Pittsburgh, Pennsylvania <br/>
+                April 11, 2026 (TBD)
+            </h3>
+        </div>
     </div>
 )
 
